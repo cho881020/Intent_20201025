a202b8d9df030362835f24c03a193b6d46d10d95
조경진 <kj_cho@idealidea.co.kr>
2020-10-25 15:54:27 +0900

    1000 숫자를 => REQ_FOR_DATA 변수로 대체.   - 가독성 향상 조치

--- app/src/main/java/kr/co/tjoeun/intent_20201025/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/intent_20201025/MainActivity.kt
@@ -6,6 +6,13 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+//    클래스 정의부분 바로 밑에 적는 (만드는) 변수 : 멤버변수
+//    fun 안에서 적는 (만드는) 변수 : 지역변수 - Local변수
+
+//    1000이라는 숫자만 보고서는 의미파악 불가. => 1000을 변수에 담아서, 의미를 이해할 수 있게 도와주자.
+    val REQ_FOR_DATA = 1000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -34,7 +41,7 @@ class MainActivity : AppCompatActivity() {
 
         changeTextBtn.setOnClickListener {
             val myIntent = Intent(this, EditDataActivity::class.java)
-            startActivityForResult(myIntent, 1000)
+            startActivityForResult(myIntent, REQ_FOR_DATA)
         }
 
     }
