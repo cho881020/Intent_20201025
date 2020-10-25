8a53e2f46c3cb5f633c5160b92e9500046b4f48f
조경진 <kj_cho@idealidea.co.kr>
2020-10-25 15:44:18 +0900

    별개화면에서 데이터 입력하기 예시용 화면 구성

---
+++ app/src/main/java/kr/co/tjoeun/intent_20201025/EditDataActivity.kt
@@ -0,0 +1,13 @@
+package kr.co.tjoeun.intent_20201025
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditDataActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_data)
+    }
+
+}
